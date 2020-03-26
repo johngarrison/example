b6fa1e6215c93d9da1ba377b8d9249bfe34baf1f
johngarrison <62122795+johngarrison@users.noreply.github.com>
2020-03-26 11:18:32 -0400

    Delete Welcome1.java

--- Welcome1.java
+++
@@ -1,9 +0,0 @@
-package org;
-
-public class Welcome1 {
-  public static void main (String[] args) {
-    System.out.print ("Welcome to");
-    System.out.println ("Java Programming!" ;
-                        }
-                        }
-                        
