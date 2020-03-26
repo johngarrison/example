45a7a9fafcbdf21ded21c3d1ab44a08078db6aeb
johngarrison <62122795+johngarrison@users.noreply.github.com>
2020-03-26 11:07:41 -0400

    Update Welcome1.java

--- Welcome1.java
+++ Welcome1.java
@@ -1,1 +1,9 @@
+package org;
 
+public class Welcome1 {
+  public static void main (String[] args) {
+    System.out.print ("Welcome to");
+    System.out.println ("Java Programming!" ;
+                        }
+                        }
+                        
