a4a5f1579f09f54fbc480817ed6f220a5f5179ff
johngarrison <62122795+johngarrison@users.noreply.github.com>
2020-04-09 21:21:52 -0400

    Create Exercise5.java

---
+++ Exercise5.java
@@ -0,0 +1,13 @@
+package org;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        int count = 0;
+         int sum = 0;
+    
+for (count = 1; count <=99; count +=2) {
+    sum += count;
+        }
+   
+ }
+}
